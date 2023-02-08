ab1a05a617da4896083ccaad840a9cc34f3ecaae
Arunan Gnanasekaran <arunang@samfundet.no>
2023-02-08 16:15:37 +0100

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
