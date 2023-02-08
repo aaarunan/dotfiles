5dd2439497e296161762936543034a7343649e49
aaarunan <arunan2212@gmail.com>
2023-02-08 18:41:41 +0100

    Add tmux tpm

--- setup.sh
+++ setup.sh
@@ -3,4 +3,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 # Plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 
