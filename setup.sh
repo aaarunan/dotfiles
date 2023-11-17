eef87d8a502f6fa31407fdef59a50bf0775e9218
Arunan Gnanasekaran <arunan2212@gmail.com>
2023-11-17 01:47:07 +0100

    Add p10k

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://git.sthu.org/repos/mutt-gruvbox.git ~/.mutt/gruvbox
 vim +PluginInstall +qall
 git config --global core.editor "vim"
